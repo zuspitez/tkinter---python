135c291e3714a01b67a2e9cf13c46119d01058b0
Elbert Ang <74346365+zuspitez@users.noreply.github.com>
2020-12-08 11:07:06 +0800

    Add files via upload

---
+++ create_window_with_label_and_button.py
@@ -0,0 +1,16 @@
+# create a window with a label #
+
+import tkinter
+
+window = tkinter.Tk()
+window.title("Face recognition system")
+window.geometry("900x500")
+
+label_1=tkinter.Label(window,text="Hello world",font=('times',40,'bold'))
+label_1.pack()
+
+button1 = tkinter.Button(window, text="click here",width=15,heigh=3,font= ('times',20))
+button1.place(x = 100, y = 200)
+
+
+window.mainloop()
